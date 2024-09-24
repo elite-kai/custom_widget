ed56c76864bc32485f8c9496a3e1e81113c8bc14
elite-kai <240552586@qq.com>
2024-09-24 23:54:59 +0800

    '测试'

---
+++ lib/custom_widget.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/widgets.dart';
+
+class CustomWidget extends StatefulWidget {
+  const CustomWidget({super.key});
+
+  @override
+  State<CustomWidget> createState() => _CustomWidgetState();
+}
+
+class _CustomWidgetState extends State<CustomWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
